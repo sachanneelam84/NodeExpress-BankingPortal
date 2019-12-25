34da0c5fe2397d038d132bc9e6554ffe51b8418f
raveesh kumar sachan <raveesh.sachan@gmail.com>
2019-12-25 12:41:50 +0530

    Complted modeule

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.post('/transfer',(req, res) => {
     accounts[req.body.to].balance = accounts[req.body.to].balance - parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-    res.render('trafer', {message:"transfer completed"});
+    res.render('trafer', {message:"Transfer Completed"});
 
 });
 app.get('/payment',(req, res) => res.render('payment', {account:accounts.credit}));
