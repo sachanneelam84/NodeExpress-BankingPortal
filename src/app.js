9aa70e0a04ab0a8656c51e97705b5b2b2c37f771
raveesh kumar sachan <raveesh.sachan@gmail.com>
2019-12-25 15:32:35 +0530

    Complted modeule

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const { writeJSON, users, accounts} = require('./data');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -10,14 +12,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
-
-const accountData = fs.readFileSync('src/json/accounts.json','utf8');
-const accounts  = JSON.parse(accountData);
-
-
-const userData = fs.readFileSync('src/json/users.json','utf8');
-const users  = JSON.parse(userData);
-
 
 app.get('/', (req, res) => res.render('index',{title:"Account Summary",accounts:accounts}));
 app.get('/savings', (req, res) => res.render('account',{account: accounts.savings}));
@@ -28,8 +22,7 @@ app.get('/transfer',(req, res) => res.render('transfer'));
 app.post('/transfer',(req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = accounts[req.body.to].balance + parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON(accounts);    
     res.render('transfer', {message:"Transfer Completed"});
 
 });
@@ -38,8 +31,7 @@ app.get('/payment',(req, res) => res.render('payment', {account:accounts.credit}
 app.post('/payment',(req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON(accounts);
     res.render('payment', {message:"payment successfull", account:accounts.credit});
 
 });
