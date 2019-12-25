d4419db1be17bb72830c83fba588e98da308c2cb
raveesh kumar sachan <raveesh.sachan@gmail.com>
2019-12-25 15:58:03 +0530

    Complted modeule

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const accountRoutes = require('./routes/accounts');
+
+const servicesRoutes = require('./routes/services');
+
 const { writeJSON, users, accounts} = require('./data');
 
 const app = express();
@@ -13,28 +17,13 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
 
+app.use('/account',accountRoutes);
+app.use('/services',servicesRoutes);
+
 app.get('/', (req, res) => res.render('index',{title:"Account Summary",accounts:accounts}));
-app.get('/savings', (req, res) => res.render('account',{account: accounts.savings}));
-app.get('/checking', (req, res) => res.render('account',{account: accounts.checking}));
-app.get('/credit', (req, res) => res.render('account',{account: accounts.credit}));
+
 app.get('/profile', (req, res) => res.render('profile',{user: users[0]}));
-app.get('/transfer',(req, res) => res.render('transfer'));
-app.post('/transfer',(req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = accounts[req.body.to].balance + parseInt(req.body.amount, 10);
-    writeJSON(accounts);    
-    res.render('transfer', {message:"Transfer Completed"});
 
-});
-app.get('/payment',(req, res) => res.render('payment', {account:accounts.credit}));
-
-app.post('/payment',(req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON(accounts);
-    res.render('payment', {message:"payment successfull", account:accounts.credit});
-
-});
 
 app.listen(3000, () => console.log('PS project is running on port 3000!'));
 
