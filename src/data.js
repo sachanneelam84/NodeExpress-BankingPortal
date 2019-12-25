83d9fdfe207dd7bfd66e1e6ecd1337d8548ee428
raveesh kumar sachan <raveesh.sachan@gmail.com>
2019-12-25 15:36:32 +0530

    Complted modeule

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const accounts  = JSON.parse(accountData);
 const userData = fs.readFileSync('src/json/users.json','utf8');
 const users  = JSON.parse(userData);
 
-const writeJSON = (accounts) => {     
+const writeJSON = () => {     
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
 };
