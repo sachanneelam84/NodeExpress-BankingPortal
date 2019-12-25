4cf44828e047376bcdebfaa088896df20423b443
raveesh kumar sachan <raveesh.sachan@gmail.com>
2019-12-25 10:38:48 +0530

    Complted modeule

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000');
+});
+
+app.get('/', function(req,res){
+res.render('index',{title:"index page"});
+
+});
+
+
