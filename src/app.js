6af2a54ad000ac796614bc12b935bc205407694d
raveesh kumar sachan <raveesh.sachan@gmail.com>
2019-12-25 11:00:16 +0530

    Complted modeule

--- src/app.js
+++ src/app.js
@@ -10,11 +10,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function() {
-    console.log('Server is listening on port 3000');
+    console.log('PS is running on port 3000');
 });
 
 app.get('/', function(req,res){
-res.render('index',{title:"index page"});
+res.render('index',{title:"Index"});
 
 });
 
